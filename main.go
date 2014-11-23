841e85deba88a0037f2ae269228198cf9a9117ac
Cedric Chin <cedric@elijames.org>
2014-11-23 13:29:25 +0700

    Add initial test helpers.

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package bgtracker
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func testHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Print(w, "Hello World")
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", testHandler)
+
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
+}
